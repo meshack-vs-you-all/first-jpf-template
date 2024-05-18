492507d200dd779a6b16bee9f35603870c3b8a53
meshack-vs-you-all <meshackmogire406@gmail.com>
2024-05-18 21:15:47 +0300

    user login and signup successful

--- src/components/Auth/Signup.jsx
+++ src/components/Auth/Signup.jsx
@@ -1,12 +1,10 @@
-// src/components/Auth/Signup.jsx
 import { useState } from 'react';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Signup = () => {
-    const [formData, setFormData] = useState({ username: '', email: '', password: '', confirmPassword: '' });
+    const [formData, setFormData] = useState({ username: '', email: '', password: '' });
     const [error, setError] = useState('');
-    const [passwordMatchError, setPasswordMatchError] = useState('');
     const navigate = useNavigate();
 
     const handleChange = (e) => {
@@ -15,77 +13,58 @@ const Signup = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if (formData.password !== formData.confirmPassword) {
-            setPasswordMatchError('Passwords do not match');
-            return;
-        }
         try {
-            await axios.post('http://localhost:8000/users/register', formData);
-            navigate('/auth');
+            const response = await axios.post('http://localhost:8000/users/register', formData);
+            localStorage.setItem('token', response.data.access_token);
+            navigate('/user');
         } catch (error) {
-            setError('Error creating account');
+            setError('Error during registration');
         }
     };
 
     return (
         <div className="flex items-center justify-center min-h-screen bg-white p-5">
-            <div className="max-w-md w-full bg-white p-8 rounded-lg shadow-md">
+            <div className="max-w-md w-full bg-white p-8 rounded-lg shadow-xl">
                 <h2 className="text-2xl font-bold mb-4">Sign Up</h2>
-                
-                {error && <p className="text-red mb-4">{error}</p>}
-                {passwordMatchError && <p className="text-red mb-4">{passwordMatchError}</p>}
+                {error && <p className="text-red-500 mb-4">{error}</p>}
                 <form onSubmit={handleSubmit} className="space-y-4">
                     <div>
-                        <label htmlFor="username" className="block text-black">Username</label>
+                        <label htmlFor="username" className="block text-gray-700">Username</label>
                         <input
                             type="text"
                             name="username"
-                            placeholder="Username"
+                            placeholder="Enter your username"
                             value={formData.username}
                             onChange={handleChange}
-                            className="w-full p-2 border border-black rounded"
+                            className="w-full p-2 border border-gray-300 rounded"
                         />
                     </div>
                     <div>
-                        <label htmlFor="email" className="block text-black">Email</label>
+                        <label htmlFor="email" className="block text-gray-700">Email</label>
                         <input
                             type="email"
                             name="email"
-                            placeholder="Enter your preferred email"
+                            placeholder="Enter your email"
                             value={formData.email}
                             onChange={handleChange}
-                            className="w-full p-2 border border-black rounded"
+                            className="w-full p-2 border border-gray-300 rounded"
                         />
                     </div>
                     <div>
-                        <label htmlFor="password" className="block text-black">Password</label>
+                        <label htmlFor="password" className="block text-gray-700">Password</label>
                         <input
                             type="password"
                             name="password"
-                            placeholder="At least 8 characters"
-
+                            placeholder="Enter your password"
                             value={formData.password}
                             onChange={handleChange}
                             className="w-full p-2 border border-gray-300 rounded"
                         />
                     </div>
-                    <div>
-                        <label htmlFor="confirmPassword" className="block text-black">Confirm Password</label>
-                        <input
-                            type="password"
-                            name="confirmPassword"
-                            placeholder="Confirm your password"
-                            value={formData.confirmPassword}
-                            onChange={handleChange}
-                            className="w-full p-2 border border-gray-300 rounded"
-                        />
-                    </div>
-                    <button type="submit" className="w-content px-4 py-2 bg-brand-primary text-white rounded hover:bg-[#61c6dd]">
-                        Sign up
+                    <button type="submit" className="w-full px-4 py-2 bg-brand-primary text-white rounded hover:bg-[#61c6dd] active:text-gray-dark">
+                        Sign Up
                     </button>
-                    <p>Your Fitness Journey starts here.</p>
                 </form>
-                
             </div>
         </div>
     );
